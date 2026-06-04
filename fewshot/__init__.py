59fe6144e5ee58da12b1af22996b72223ae54743
Yeping Zhao <zyp2606841563@gmail.com>
2026-06-04 15:46:27 +0800

    refactor: move _fewshot.py content to fewshot/content.py

--- fewshot/__init__.py
+++ fewshot/__init__.py
@@ -1,1 +1,6 @@
-"""Few-shot in-context learning for VLM evaluation."""
+"""Few-shot in-context learning for VLM evaluation.
+
+Modules:
+  - sampler: example selection from the training set
+  - content: shared content-block construction for few-shot prompts
+"""
